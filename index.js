87f3215d6213cffa90046c9b2ae74cc263281def
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-10-03 21:21:24 +0700

    Add scroll up and down behavior for the floating buttons

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const floatingButtons = {
         floatingButtons.floatingButtonsElements[0].className = document.documentElement.scrollTop >= bodyHeightReference ? "floating-button-active" : "";
         floatingButtons.floatingButtonsElements[1].className = areaNodes[3].getBoundingClientRect().top <= bodyHeightReference ? "" : "floating-button-active";
     },
-    // PROG this already works as intended
-    // CMT add this when clicking the buttons too (i think i should call this AFTER it has scrolled to the area so that i will update to the current area)
     checkScrollLoc: function () {
         for (let i = areaNodes.length - 1; i >= 0; i--) {
             if (areaNodes[i].getBoundingClientRect().top <= 0) {
@@ -20,17 +18,18 @@ const floatingButtons = {
             } // Whenever this is checked, iterate through all #area nodes starting from the end. If a node has their above border hit or exceed the above viewport (<= 0), change floatingButtons.currentScrollLoc to the index (i) of that node and breaks the loop so that variable i will refer to the node whose above border exceeded closest from the top of the viewport.
         }
     },
-    // CMT remmber to use scrollintoview
-    // PROG do this tomorrow
-    clickScrollUp: function () {
-        ;
+    // FIXME sometimes the scrolling stops midway
+    // TODO add spam prevention
+    scrollUp: function () {
+        areaNodes[floatingButtons.currentScrollLoc].getBoundingClientRect().top === 0 ? areaNodes[floatingButtons.currentScrollLoc - 1].scrollIntoView(true) : areaNodes[floatingButtons.currentScrollLoc].scrollIntoView(true);
     },
-    clickScrollDown: function () {
-        ;
+    scrollDown: function () {
+        areaNodes[floatingButtons.currentScrollLoc + 1].scrollIntoView(true);
     },
 };
-
 window.addEventListener("scroll", floatingButtons.growFloatButtons, false);
 window.addEventListener("scroll", floatingButtons.checkScrollLoc, false);
+floatingButtons.floatingButtonsElements[0].addEventListener("click", floatingButtons.scrollUp, false);
+floatingButtons.floatingButtonsElements[1].addEventListener("click", floatingButtons.scrollDown, false);
 
 // #endregion floating button grow
