9879fad7685e51426a1ebc520b82f58e8bab21d6
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-10-12 22:56:05 +0700

    Refactor sortTable()

--- scripts/sort-table.js
+++ scripts/sort-table.js
@@ -1,25 +1,21 @@
+// TODO add this new version to my library
 /**
- * This function will sort a table rows based on the content of a column in the tbody of the table.
+ * This function will sort a table rows based on the content of a column in the tbody of the table. This function will return a function that will do the sorting, so it can be assigned to a handler or somewhere else.
  * @param {string} whichTable - The id or class of the table element of the table
- * @param {Number} whichTableDataColumn - The column of the table. The column is based on the order of the td in each tr in the tbody.
+ * @param {Number} whichTableColumn - The column of the table. The column is based on the order of the td in each tr in the tbody.
  * @param {string} ascOrDesc - Takes  "desc" to sort descending, else it will sort ascending.
  * @param {string} numberOrString - Takes  "string" to sort the column based on string data type, else it will sort based on numbers. The data type of each cell in the column can only be all strings or all numbers, no mixing allowed for this function to work.
  * @returns function
  */
-function sortTable(whichTable, whichTableDataColumn, ascOrDesc = "asc", numberOrString = "number") {
-	/**
-	 * @type {Array}
-	 */
-	const tableDataColumn = [...document.querySelectorAll(whichTable + " tbody td:nth-of-type(" + whichTableDataColumn + ")")];
+function sortTable(whichTable, whichTableColumn, ascOrDesc = "asc", numberOrString = "number") {
+	const tableColumn = [...document.querySelectorAll(whichTable + " tbody td:nth-of-type(" + whichTableColumn + ")")];
 	const tableBody = document.querySelector(whichTable + " tbody");
-	const tableDataColumnRow = []; // Array to store the parent (the tr) of each td that was sorted earlier.
-	tableDataColumn.sort(function (item1, item2) {
+	const tableRow = []; // Array to store the parent (the tr) of each td that was sorted.
+	tableColumn.sort(function (item1, item2) {
 		if (numberOrString == "number") {
-			if (ascOrDesc == "asc") {
-				return parseFloat(item1.innerText) - parseFloat(item2.innerText);
-			} else {
-				return parseFloat(item2.innerText) - parseFloat(item1.innerText);
-			}
+			return ascOrDesc == "asc"
+				? parseFloat(item1.innerText) - parseFloat(item2.innerText)
+				: parseFloat(item2.innerText) - parseFloat(item1.innerText);
 		} else {
 			if (ascOrDesc == "asc") {
 				if (item1.innerText > item2.innerText) {
@@ -40,13 +36,12 @@ function sortTable(whichTable, whichTableDataColumn, ascOrDesc = "asc", numberOr
 			}
 		}
 	});
-	for (let eachCellsRow of tableDataColumn) {
-		tableDataColumnRow.push(eachCellsRow.parentElement); // For each item which is a reference value to a td that has been sorted, get it's parent and push it to the array. The ending result will be an array full of reference value to the tr in a sorted manner.
+	for (let eachCellsRow of tableColumn) {
+		tableRow.push(eachCellsRow.parentElement); // For each item which is a reference value to a td that has been sorted, get its parent and push it to the array. The ending result will be an array full of reference value to the table's tr that is sorted.
 	}
 	return function () {
-		// Return a function reference with the enviroment(s) of its closure so that it can be used for example in a handler.
-		for (let eachGonnaBeInsertedRows of tableDataColumnRow) {
-			tableBody.insertAdjacentElement("beforeend", eachGonnaBeInsertedRows); // For each row, insert it as the last child of the tbody. Since the row is already in order, this will reorder each trs' current DOM position in the tbody (or its position visually in the viewport) to the new DOM position based off the items in tableDataColumnRow by appending each one before the end of the tbody children tree layer.
+		for (let row of tableRow) {
+			tableBody.insertAdjacentElement("beforeend", row); // For each row, insert it as the last child of the tbody. Since the rows are already in order, this will reorder the trs either ascending or descending.
 		}
 	};
 }
