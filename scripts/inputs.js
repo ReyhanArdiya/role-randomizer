9f3d60a79e6fa3b0dcd7f774d05ccfa2250710d5
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-10-14 19:41:01 +0700

    Move adding input rows code into an object

--- scripts/inputs.js
+++ scripts/inputs.js
@@ -1,20 +1,21 @@
 //@ts-check
-const addMoreButtons = document.querySelectorAll(".add-more-button");
-const inputTemplates = document.querySelectorAll("template");
+const inputRows = {
+	addMoreButtons: document.querySelectorAll(".add-more-button"),
+	inputTemplates: document.querySelectorAll("template"),
+	/**
+	 * @param {number} whichTemplate
+	 * @param {string} whichInputTable
+	 */
+	addRows: function (whichTemplate, whichInputTable) {
+		const content = this.inputTemplates[whichTemplate].content;
+		return function () {
+			const clone = content.cloneNode(true);
+			document.querySelector(whichInputTable + " tbody").appendChild(clone);
+		};
+	}
+};
 
-/**
- * @param {number} whichTemplate
- * @param {string} whichInputTable
- */
-function addRows(whichTemplate, whichInputTable) {
-	const content = inputTemplates[whichTemplate].content;
-	return function () {
-		const clone = content.cloneNode(true);
-		document.querySelector(whichInputTable + " tbody").appendChild(clone);
-	};
-}
-
-for (let i = 0; i < addMoreButtons.length; i++) {
-	addMoreButtons[i].addEventListener("click", addRows(0, "#heading-members"), false);
-	addMoreButtons[i].addEventListener("click", addRows(1, "#heading-roles"), false);
+for (let i = 0; i < inputRows.addMoreButtons.length; i++) {
+	inputRows.addMoreButtons[i].addEventListener("click", inputRows.addRows(0, "#heading-members"), false);
+	inputRows.addMoreButtons[i].addEventListener("click", inputRows.addRows(1, "#heading-roles"), false);
 }
