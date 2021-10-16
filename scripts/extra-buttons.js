2aa1f051e5827fc9a5b806ab03a2fd10100f32ed
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-10-16 21:11:15 +0700

    Add copyToClipboardButton object

--- scripts/extra-buttons.js
+++ scripts/extra-buttons.js
@@ -2,3 +2,23 @@
 "use strict";
 
 const extraButtons = document.querySelectorAll("#area-result > footer button");
+
+const copyToClipboardButton = {
+	resultAnnouncement: "",
+	makeResultAnnouncement: function (explanationStr = "role is") {
+		let joinedAnnouncement = inputData.results.map(function (/** @type {[]} */ memberAndRoleArr) {
+			return memberAndRoleArr.join(` ${explanationStr} `);
+		});
+		console.log(joinedAnnouncement);
+	},
+	copyToClipBoard: function (resultAnnouncement) {
+		navigator.clipboard.writeText(resultAnnouncement);
+	}
+};
+/** Example announcement, each member is in their own line
+ * Adam role is makalah
+ * Bea role is ppt
+ * member role is role
+ * results[0] role is results[1]
+ */
+// syntax: "1\n2\n3\n4" where \n is a new linebreak
