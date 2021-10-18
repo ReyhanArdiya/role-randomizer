330b9f93dd84df8070980ae8e93a0aaec8a8dc6e
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-10-18 22:33:03 +0700

    Add typing to sort-table.js

--- scripts/sort-table.js
+++ scripts/sort-table.js
@@ -1,16 +1,26 @@
 // TODO add this new version to my library
 /**
- * This function will sort a table rows based on the content of a column in the tbody of the table. This function will return a function that will do the sorting, so it can be assigned to a handler or somewhere else.
- * @param {string} whichTable - The id or class of the table element of the table
- * @param {Number} whichTableColumn - The column of the table. The column is based on the order of the td in each tr in the tbody.
- * @param {string} ascOrDesc - Takes  "desc" to sort descending, else it will sort ascending.
- * @param {string} numberOrString - Takes  "string" to sort the column based on string data type, else it will sort based on numbers. The data type of each cell in the column can only be all strings or all numbers, no mixing allowed for this function to work.
- * @returns function
+ * This function will sort a table rows based on the content of a column in the tbody of the table. This function will return a handler that will do the sorting, so it can be assigned to an element.
+ * @param {string} whichTable - The id or class of the table element of the table.
+ * @param {number} whichTableColumn - The column of the table. The column is based on the order of the td in each tr in the tbody.
+ * @param {string} [ascOrDesc="asc"] - Takes  "desc" to sort descending or "asc" to sort ascending; default is "asc".
+ * @param {string} [numberOrString="number"] - Takes  "string" to sort the column based on string data type or "number" to sort based on numbers; default is "number". The data type of each cell in the column can only be all strings or all numbers, no mixing allowed for this function to work. But combining a number and a string in a string works, e.g.: "$0.75".
+ * @returns {EventListener} Handler that will do the sorting.
  */
 function sortTable(whichTable, whichTableColumn, ascOrDesc = "asc", numberOrString = "number") {
-	const tableColumn = [...document.querySelectorAll(`${whichTable} tbody td:nth-of-type(${whichTableColumn})`)];
+	/**
+	 * Spreaded node list of td elements in the selected table's column {@link whichTableColumn}.
+	 * @type {HTMLTableCellElement[]}
+	 */
+	const tableColumn = /**@type {HTMLTableCellElement[]}*/ ([
+		...document.querySelectorAll(`${whichTable} tbody td:nth-of-type(${whichTableColumn})`)
+	]);
 	const tableBody = document.querySelector(`${whichTable} tbody`);
-	const tableRow = []; // Array to store the parent (the tr) of each td that was sorted.
+	/**
+	 * Array to store the parent (the tr elements) of each td that was sorted.
+	 * @type {HTMLTableRowElement[]}
+	 */
+	const tableRow = [];
 	tableColumn.sort(function (item1, item2) {
 		if (numberOrString == "number") {
 			return ascOrDesc == "asc"
@@ -37,15 +47,19 @@ function sortTable(whichTable, whichTableColumn, ascOrDesc = "asc", numberOrStri
 		}
 	});
 	for (let eachCellsRow of tableColumn) {
-		tableRow.push(eachCellsRow.parentElement); // For each item which is a reference value to a td that has been sorted, get its parent and push it to the array. The ending result will be an array full of reference value to the table's tr that is sorted.
+		tableRow.push(/**@type {HTMLTableRowElement}*/ (eachCellsRow.parentElement)); // For each item which is a reference value to a td that has been sorted, get its parent and push it to the array. The ending result will be an array full of reference value to the table's tr that is sorted.
 	}
 	return function () {
 		for (let row of tableRow) {
-			tableBody.insertAdjacentElement("beforeend", row); // For each row, insert it as the last child of the tbody. Since the rows are already in order, this will reorder the trs either ascending or descending.
+			tableBody?.insertAdjacentElement("beforeend", row); // For each row, insert it as the last child of the tbody. Since the rows are already in order, this will reorder the trs either ascending or descending.
 		}
 	};
 }
 
+/**
+ * Function to add the sort handlers from {@link sortTable} to the sorting buttons in the results table header.
+ * @returns {void}
+ */
 function addSortToButtons() {
 	const paths = document.querySelectorAll("#results-container path");
 	let column = 1;
