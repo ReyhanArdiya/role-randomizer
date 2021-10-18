adc35e2d69302fd9d794b41f7bfd224e5e2a6290
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-10-18 11:49:46 +0700

    Add JSDoc typing to extra-buttons.js

--- scripts/extra-buttons.js
+++ scripts/extra-buttons.js
@@ -1,19 +1,41 @@
-//@ts-check
 "use strict";
 
+/**
+ * Contains NodeList of the 3 buttons in the results area
+ * @type {NodeListOf<HTMLButtonElement>}
+ * @property {HTMLButtonElement} 0 - copy to clipboard button
+ * @property {HTMLButtonElement} 1 - retry button
+ * @property {HTMLButtonElement} 2 - restart button
+ */
 const extraButtons = document.querySelectorAll("#area-result > footer button");
 
 // #region copy to clipboard button
 
+/**
+ * Object to handle copying to user's clipboard.
+ */
 const copyToClipboardButton = {
+	/**
+	 * String of the randomizing results assigned by {@link copyToClipboardButton.makeResultsAnnouncement}.
+	 * @type {string}
+	 */
 	resultsAnnouncement: "",
 	// FIXME this works, but we should make the results based on the CURRENT TABLE BEING DISPLAYED IN THE ROLES AREA  so that it can copy based on the sorting position. This one doesn't copy based on the sort positions
+	/**
+	 * Method to make the results announcement and assign it to {@link copyToClipboardButton.resultsAnnouncement}.
+	 * @param {string} [explanationStr="role is"] - A string that will be used in the middle of the concatenated strings.
+	 */
 	makeResultsAnnouncement: function (explanationStr = "role is") {
-		const joinedAnnouncement = inputData.results.map(function (/** @type {[]} */ memberAndRoleArr) {
+		const joinedAnnouncement = inputData.results?.map(function (/** @type {[string, string]} */ memberAndRoleArr) {
 			return memberAndRoleArr.join(` ${explanationStr} `);
 		});
-		copyToClipboardButton.resultsAnnouncement = joinedAnnouncement.join("\n");
+		// @ts-ignore
+		copyToClipboardButton.resultsAnnouncement = joinedAnnouncement?.join("\n");
 	},
+	/**
+	 * Method to copy a string to the user's clipboard.
+	 * @param {string} resultsAnnouncement - A string that will be coppied, usually is taken from {@link copyToClipboardButton.resultsAnnouncement}.
+	 */
 	copyToClipBoard: function (resultsAnnouncement) {
 		navigator.clipboard.writeText(resultsAnnouncement);
 	}
